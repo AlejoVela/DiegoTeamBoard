e27d61012a4cc03dfae853103c45d3e99e589a22
Diego R <alejorvela@gmail.com>
2021-08-25 17:55:53 -0500

    complete configuration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,74 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListRoleComponent } from './admin/list-role/list-role.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { RegisterRoleComponent } from './admin/register-role/register-role.component';
+import { RegisterUserComponent } from './admin/register-user/register-user.component';
+import { UpdateRoleComponent } from './admin/update-role/update-role.component';
+import { UpdateUserComponent } from './admin/update-user/update-user.component';
+import { ListTaskComponent } from './board/list-task/list-task.component';
+import { SaveTaskComponent } from './board/save-task/save-task.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterComponent } from './home/register/register.component';
+
+//services
+import { UserService } from './services/user.service';
+import { BoardService } from './services/board.service';
+import { RoleService } from './services/role.service';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+//guards
+import { AuthGuard } from './guard/auth.guard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListRoleComponent,
+    ListUserComponent,
+    RegisterRoleComponent,
+    RegisterUserComponent,
+    UpdateRoleComponent,
+    UpdateUserComponent,
+    ListTaskComponent,
+    SaveTaskComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    UserService,
+    BoardService,
+    RoleService,
+    TokenInterceptorService,
+    AuthGuard,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
